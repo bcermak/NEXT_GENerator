c1bba5643acf443bc1084304c6302f3eb7aa1dcb
Brandon Cermak <cermakprofessional@gmail.com>
2021-10-01 12:43:27 -0500

    added new CSS and some fun audio

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,22 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
 
+var audio = new Audio('assets/meow.mp3');
+
+
 // Write password to the #password input
 function writePassword() {
+  audio.play();
   let lengthQ = prompt("How long would you like your password to be?");
  
   if (lengthQ < 8 || lengthQ > 128  ) {
     alert("Not in required length range!");
   } else {
-    let lowerQ = prompt("Would you like to use lowercase characters?");
-    let upperQ = prompt("How about uppercase?");
-    let specQ = prompt("Do you want your characters to be 'special'?")
+    let lowerQ = prompt("Would you like to use lowercase characters? (Y/N)");
+    let upperQ = prompt("How about uppercase? (Y/N)");
+    let specQ = prompt("Do you want your characters to be 'special' (Y/N)?")
     console.log(lengthQ, lowerQ, upperQ, specQ)
   }
-
-
-
 
   let password = generatePassword();
   let passwordText = document.querySelector("#password");
